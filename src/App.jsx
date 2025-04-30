babe1f7b1e893db5b9c4493d9900d790a708a9b3
qudsiyashaikh <zinharkhan8@Gmail.com>
2025-04-30 21:04:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AgeCalculator from "./Components/Age Calculator"
+function App() {
+  return (
+    <div className="">
+      <AgeCalculator />
+
+    </div>
+  );
+}
+
+export default App;
+
